5a777b8689bdff7ed3ec59eb09c31c6569d7014e
alan Zhang <zhj.buffer@gmail.com>
2021-12-28 20:56:38 +0800

    update uart driver code
    Signed-off-by: alan Zhang <zhj.buffer@gmail.com>

--- ros2_rada_driver/include/rada_uart.hpp
+++ ros2_rada_driver/include/rada_uart.hpp
@@ -5,433 +5,127 @@
 extern "C" {
 //#endif // __cplusplus
 
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<assert.h>
-#include<termios.h>
-#include<string.h>
-#include<sys/time.h>
-#include<sys/types.h>
-#include<errno.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
-//#include "uart.h"
-static int ret;
-
-
-#if 1
- int uart_init(char *path);
- int uart_deinit(int fd);
- int uart_close(int fd);
- int uart_write(int fd,const  char *w_buf,size_t len);
- int uart_read(int fd, char *r_buf,size_t len);
- int LobotSerialWrite(int fd,  char *buf, size_t len);
- int LobotSerialRead(int fd,  char *buf, size_t len);
-
-#endif
-
-/*
- * 安全读写函数
- */
-
-ssize_t safe_write(int fd, const  char *vptr, size_t n)
-{
-    size_t  nleft;
-    ssize_t nwritten;
-    const  char *ptr;
-
-    ptr = vptr;
-    nleft = n;
-
-    while(nleft > 0)
-    {
-    if((nwritten = write(fd, ptr, nleft)) <= 0)
-        {
-            if(nwritten < 0&&errno == EINTR)
-                nwritten = 0;
-            else
-                return -1;
-        }
-        nleft -= nwritten;
-        ptr   += nwritten;
-    }
-    return(n);
-}
-
-ssize_t safe_read(int fd,  char *vptr,size_t n)
-{
-    size_t nleft;
-    ssize_t nread;
-     char *ptr;
-
-    ptr=vptr;
-    nleft=n;
-
-    while(nleft > 0)
-    {
-        if((nread = read(fd,ptr,nleft)) < 0)
-        {
-            if(errno == EINTR)//被信号中断
-                nread = 0;
-            else
-                return -1;
-        }
-        else
-        if(nread == 0)
-            break;
-
-	if (*ptr != 0xff) {
-		printf("not header %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x\n", *ptr, *(ptr+1), *(ptr+2) ,*(ptr+3) ,*(ptr+4) ,*(ptr+5) ,*(ptr+6) ,*(ptr+7) ,*(ptr+8) ,*(ptr+9));
-		for (int i = 1; i < 10; i++)
-		{
-			if (*(ptr+i) == 0xff) {
-				memmove(ptr, ptr+i, 10 - i);
-
-//				int ret = read(fd, ptr+, i);
-//				memcpy(ptr+10-i, buf, i);
-				nread = 10 - i;
-			}
-
-			if ((i == 9) && (*(ptr+9) != 0xff))
-				printf("no header found\n");
-		}
-
-	} else {
-	}
-
-
-        nleft -= nread;
-        ptr += nread;
-    }
-    return (n-nleft);
-}
-
-int uart_open(int fd,const char *pathname)
-{
-    assert(pathname);
-
-    /*打开串口*/
-    fd = open(pathname,O_RDWR|O_NOCTTY|O_NDELAY|O_NONBLOCK);
-    if(fd == -1)
-    {
-        perror("Open UART failed!");
-        return -1;
-    }
-
-    /*清除串口非阻塞标志*/
-    if(fcntl(fd,F_SETFL,0) < 0)
-    {
-        fprintf(stderr,"fcntl failed!\n");
-        return -1;
-    }
-
-    return fd;
-}
-
-int uart_set(int fd,int baude,int c_flow,int bits, char parity,int stop)
-{
-    struct termios options;
-
-    /*获取终端属性*/
-    if(tcgetattr(fd,&options) < 0)
-    {
-        perror("tcgetattr error");
-        return -1;
-    }
-
-
-    /*设置输入输出波特率，两者保持一致*/
-    switch(baude)
-    {
-        case 4800:
-            cfsetispeed(&options,B4800);
-            cfsetospeed(&options,B4800);
-            break;
-        case 9600:
-            cfsetispeed(&options,B9600);
-            cfsetospeed(&options,B9600);
-            break;
-        case 19200:
-            cfsetispeed(&options,B19200);
-            cfsetospeed(&options,B19200);
-            break;
-        case 38400:
-            cfsetispeed(&options,B38400);
-            cfsetospeed(&options,B38400);
-            break;
-        case 115200:
-            cfsetispeed(&options,B115200);
-            cfsetospeed(&options,B115200);
-            break;
-        default:
-            fprintf(stderr,"Unkown baude!\n");
-            return -1;
-    }
-
-    /*设置控制模式*/
-    options.c_cflag |= CLOCAL;//保证程序不占用串口
-    options.c_cflag |= CREAD;//保证程序可以从串口中读取数据
-
-    /*设置数据流控制*/
-    switch(c_flow)
-    {
-        case 0://不进行流控制
-            options.c_cflag &= ~CRTSCTS;
-            break;
-        case 1://进行硬件流控制
-            options.c_cflag |= CRTSCTS;
-            break;
-        case 2://进行软件流控制
-            options.c_cflag |= IXON|IXOFF|IXANY;
-            break;
-        default:
-            fprintf(stderr,"Unkown c_flow!\n");
-            return -1;
-    }
-
-    /*设置数据位*/
-    switch(bits)
-    {
-        case 5:
-            options.c_cflag &= ~CSIZE;//屏蔽其它标志位
-            options.c_cflag |= CS5;
-            break;
-        case 6:
-            options.c_cflag &= ~CSIZE;//屏蔽其它标志位
-            options.c_cflag |= CS6;
-            break;
-        case 7:
-            options.c_cflag &= ~CSIZE;//屏蔽其它标志位
-            options.c_cflag |= CS7;
-            break;
-        case 8:
-            options.c_cflag &= ~CSIZE;//屏蔽其它标志位
-            options.c_cflag |= CS8;
-            break;
-        default:
-            fprintf(stderr,"Unkown bits!\n");
-            return -1;
-    }
-
-    /*设置校验位*/
-    switch(parity)
-    {
-        /*无奇偶校验位*/
-        case 'n':
-        case 'N':
-            options.c_cflag &= ~PARENB;//PARENB：产生奇偶位，执行奇偶校验
-            options.c_cflag &= ~INPCK;//INPCK：使奇偶校验起作用
-            break;
-        /*设为空格,即停止位为2位*/
-        case 's':
-        case 'S':
-            options.c_cflag &= ~PARENB;//PARENB：产生奇偶位，执行奇偶校验
-            options.c_cflag &= ~CSTOPB;//CSTOPB：使用两位停止位
-            break;
-        /*设置奇校验*/
-        case 'o':
-        case 'O':
-            options.c_cflag |= PARENB;//PARENB：产生奇偶位，执行奇偶校验
-            options.c_cflag |= PARODD;//PARODD：若设置则为奇校验,否则为偶校验
-            options.c_cflag |= INPCK;//INPCK：使奇偶校验起作用
-            options.c_cflag |= ISTRIP;//ISTRIP：若设置则有效输入数字被剥离7个字节，否则保留全部8位
-            break;
-        /*设置偶校验*/
-        case 'e':
-        case 'E':
-            options.c_cflag |= PARENB;//PARENB：产生奇偶位，执行奇偶校验
-            options.c_cflag &= ~PARODD;//PARODD：若设置则为奇校验,否则为偶校验
-            options.c_cflag |= INPCK;//INPCK：使奇偶校验起作用
-            options.c_cflag |= ISTRIP;//ISTRIP：若设置则有效输入数字被剥离7个字节，否则保留全部8位
-            break;
-        default:
-            fprintf(stderr,"Unkown parity!\n");
-            return -1;
-    }
-
-    /*设置停止位*/
-    switch(stop)
-    {
-        case 1:
-            options.c_cflag &= ~CSTOPB;//CSTOPB：使用两位停止位
-            break;
-        case 2:
-            options.c_cflag |= CSTOPB;//CSTOPB：使用两位停止位
-            break;
-        default:
-            fprintf(stderr,"Unkown stop!\n");
-            return -1;
-    }
-
-    /*设置输出模式为原始输出*/
-    options.c_oflag &= ~OPOST;//OPOST：若设置则按定义的输出处理，否则所有c_oflag失效
-
-    /*设置本地模式为原始模式*/
-    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    //options.c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN|ECHOE);
-    /*
-     *ICANON：允许规范模式进行输入处理
-     *ECHO：允许输入字符的本地回显
-     *ECHOE：在接收EPASE时执行Backspace,Space,Backspace组合
-     *ISIG：允许信号
-     */
-
-    /*设置等待时间和最小接受字符*/
-    options.c_cc[VTIME] = 0;//可以在select中设置
-    options.c_cc[VMIN] = 1;//最少读取一个字符
-
-    /*如果发生数据溢出，只接受数据，但是不进行读操作*/
-    tcflush(fd,TCIFLUSH);
-
-    /*激活配置*/
-    if(tcsetattr(fd,TCSANOW,&options) < 0)
-    {
-        perror("tcsetattr failed");
-        return -1;
-    }
-
-    return 0;
-}
-
-int uart_read(int fd, char *r_buf,size_t len)
-{
-    ssize_t cnt = 0;
-    fd_set rfds;
-    struct timeval time;
-
-    /*将文件描述符加入读描述符集合*/
-    FD_ZERO(&rfds);
-    FD_SET(fd,&rfds);
-
-    /*设置超时为15s*/
-    time.tv_sec = 15;
-    time.tv_usec = 0;
-
-        //printf("fd: %d %s, %s, %d\n",fd, __func__,__FILE__,__LINE__);
-    /*实现串口的多路I/O*/
-    ret = select(fd+1,&rfds,NULL,NULL,&time);
-    switch(ret)
-    {
-        case -1:
-            fprintf(stderr,"select error!\n");
-            return -1;
-        case 0:
-            fprintf(stderr,"time over!\n");
-            return -1;
-        default:
-            cnt = safe_read(fd,r_buf,len);
-            if(cnt == -1)
-            {
-                fprintf(stderr,"read error!\n");
-                return -1;
-            }
-            return cnt;
-    }
-}
-
-int uart_write(int fd,const  char *w_buf,size_t len)
-{
-    ssize_t cnt = 0;
-
-        //printf("fd: %d %s, %s, %d\n",fd, __func__,__FILE__,__LINE__);
-    cnt = safe_write(fd,w_buf,len);
-    if(cnt == -1)
-    {
-        fprintf(stderr,"write error!\n");
-        return -1;
-    }
-
-    return cnt;
-}
-
-int uart_close(int fd)
-{
-	//printf(" fd: %d \n", fd);
-    assert(fd);
-    close(fd);
-
-    /*可以在这里做些清理工作*/
-
-    return 0;
-}
-
-int uart_deinit(int fd)
-{
-	return uart_close(fd);
-}
-int uart_init(char *path)
-{
-
-    int fd = -1;
-
-    fd = uart_open(fd,path);/*串口号/dev/ttySn,USB口号/dev/ttyUSBn*/
-    if(fd == -1)
-    {
-        fprintf(stderr,"uart_open error\n");
-        exit(EXIT_FAILURE);
-    }
-
-        //printf("fd: %d %s, %s, %d\n",fd, __func__,__FILE__,__LINE__);
-    if(uart_set(fd,9600,0,8,'N',1) == -1)
-    {
-        fprintf(stderr,"uart set failed!\n");
-        exit(EXIT_FAILURE);
-        return -1;
-    }
-
-
-    return fd;
-}
-
-//int uartWriteBuf( char *buf, size_t len)
-int LobotSerialWrite(int fd,  char *buf, size_t len)
-{
-
-    //printf("%s%s%d\n", __func__,__FILE__, __LINE__);
-    return uart_write(fd, buf, len);
-
-}
-
-//int uartReadBuf( char *buf, size_t len)
-int LobotSerialRead(int fd,  char *buf, size_t len)
-{
-
-    return uart_read(fd, buf, len);
-
-}
-
-/*********************************************************************************
- *  * Function:  receiveHandle
- *   * Description: ??????,??????,????
- *    * Parameters:   ?????
- *     * Return:       ???
- *      * Others:
- *       **********************************************************************************/
-
-//static uint8_t UART_RX_BUF[24];
-static bool isUartRxCompleted = false;
-
-
-bool isRxCompleted(void)
-{
-#if 1
-    if(isUartRxCompleted)
-    {
-        isUartRxCompleted = false;
-        return true;
-
-    }
-    else
-    {
-        return false;
-
-    }
-#endif
-}
+#include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ int uart_close(int Uart_fd);
+ int uart_read(int Uart_fd, unsigned char *pData, int max_size);
+ int uart_write(int Uart_fd, unsigned char *pData, int max_size);
+ int uart_setup(int Uart_fd, int baud_rate);
+ int uart_open(char *pDevPath);
+
+ int uart_close(int Uart_fd)
+ {
+	 if(Uart_fd > 0)
+	 {
+		 /* Clear the data in FIFO */
+		 tcflush(Uart_fd, TCIFLUSH);
+		 close(Uart_fd);
+		 Uart_fd = 0;
+	 }
+
+	 return Uart_fd;
+ }
+
+ int uart_read(int Uart_fd, unsigned char *pData, int max_size)
+ {
+	 int ret = 0;
+
+	 if(Uart_fd > 0)
+	 {
+		 ret = read(Uart_fd, pData, max_size);
+
+		 if (ret < 0)
+		 {
+			 printf("Failed(%d:%s) to read from uart\n", errno, strerror(errno));
+		 }
+	 }
+	 return ret;
+ }
+
+ int uart_write(int Uart_fd, unsigned char *pData, int max_size)
+ {
+	 int ret = 0;
+	 if (Uart_fd > 0) {
+		 ret = write(Uart_fd, pData, max_size);
+		 if (ret < 0)
+			 perror("write data");
+		 else
+			 printf("Write %d bytes done.\n", max_size);
+	 }
+	 return ret;
+ }
+ int uart_setup(int Uart_fd, int baud_rate)
+ {
+	 int    ret = 0;
+	 struct termios termios_now;
+	 struct termios termios_new;
+	 struct termios termios_chk;
+
+	 if( Uart_fd > 0 )
+	 {
+		 memset( &termios_now, 0x0, sizeof(struct termios));
+		 memset( &termios_new, 0x0, sizeof(struct termios));
+		 memset( &termios_chk, 0x0, sizeof(struct termios));
+	 }
+
+	 tcflush(Uart_fd, TCIOFLUSH);
+	 tcgetattr(Uart_fd, &termios_new);
+	 termios_new.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	 termios_new.c_oflag &= ~OPOST;
+	 termios_new.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	 termios_new.c_cflag &= ~(CSIZE | PARENB);
+	 termios_new.c_cflag |= CS8;
+	 termios_new.c_cflag &= ~CRTSCTS;
+	 termios_new.c_cc[VTIME] = 0; /* Over than VTIME*0.1 second(s), read operation will return from blocked */
+	 termios_new.c_cc[VMIN]  = 1; /* Over than VMIN bytes received, read operation will return from blocked */
+	 tcsetattr(Uart_fd, TCSANOW, &termios_new);
+	 tcflush(Uart_fd, TCIOFLUSH);
+	 tcsetattr(Uart_fd, TCSANOW, &termios_new);
+	 tcflush(Uart_fd, TCIOFLUSH);
+	 tcflush(Uart_fd, TCIOFLUSH);
+	 cfsetospeed(&termios_new, baud_rate);
+	 cfsetispeed(&termios_new, baud_rate);
+	 tcsetattr(Uart_fd, TCSANOW, &termios_new);
+
+	 if ( ret == 0 )
+	 {
+		 /* Write back finished, so read the settings again to check the content */
+		 ret = tcgetattr( Uart_fd, &termios_chk );
+		 if( ret == 0 )
+		 {
+			 if( termios_new.c_cflag != termios_chk.c_cflag )
+			 {
+				 printf("Failed to set the parameters of termios\n");
+			 } else
+			 {
+				 printf("Set the paremeters successfully\n");
+				 ret = 0;
+			 }
+		 }
+	 }
+	 return ret;
+ }
+ int uart_open(char *pDevPath)
+ {
+	
+	int  Uart_fd = open( pDevPath, O_RDWR);
+	 if ( Uart_fd < 0 )
+	 {
+		 printf("Failed(%d:%s) to open uart(%s).\n", errno, strerror(errno), pDevPath);
+		 return -1;
+	 }
+	 return Uart_fd;
+ }
 
 
 //#ifdef __cplusplus
--- ros2_rada_driver/src/rada_pub.cpp
+++ ros2_rada_driver/src/rada_pub.cpp
@@ -20,22 +20,21 @@ class RadaPublisher : public rclcpp::Node
     : Node("RadaPublisher")
     {
 
-      char w_buf[8] = {0x01, 0x06, 0x02, 0x02, 0x00, 0x01, 0xe8, 0x72};
+      unsigned char  w_buf[8] = {0x01, 0x06, 0x02, 0x02, 0x00, 0x01, 0xe8, 0x72};
       size_t w_len = sizeof(w_buf);
-      char r_buf[10] = {0};
+      unsigned char r_buf[10] = {0};
+      int ret = -1;
 
 #if 1
-      char *tty1 = "/dev/ttyUSB4";
-      fd1= uart_init(tty1);
-      tcflush(fd1,TCIOFLUSH);
+      fd1 = uart_open("/dev/ttyUSB4");
+      uart_setup(fd1, B9600);
       ret = uart_write(fd1,w_buf,w_len);
       if(ret == -1)
       {
 	      fprintf(stderr,"uart write failed!\n");
 	      exit(EXIT_FAILURE);
       }
-      tcflush(fd1,TCIFLUSH);
-      ret = uart_read(fd1,r_buf,8);
+      ret = uart_read(fd1,r_buf,w_len);
       if(ret == -1)
       {
 	      fprintf(stderr,"uart read failed!\n");
@@ -44,29 +43,26 @@ class RadaPublisher : public rclcpp::Node
 
       publisher1_ = this->create_publisher<ros2_rada_msg::msg::Rada>("rada1", 10);
       timer1_ = this->create_wall_timer(
-		      200ms, std::bind(&RadaPublisher::timer_callback1, this));
+		      100ms, std::bind(&RadaPublisher::timer_callback1, this));
 #endif
 #if 1
-      char *tty2 = "/dev/ttyUSB5";
-      fd2= uart_init(tty2);
-      tcflush(fd2,TCIOFLUSH);
+      fd2 = uart_open("/dev/ttyUSB5");
+      uart_setup(fd2, B9600);
       ret = uart_write(fd2,w_buf,w_len);
       if(ret == -1)
       {
 	      fprintf(stderr,"uart write failed!\n");
 	      exit(EXIT_FAILURE);
       }
-      tcflush(fd2,TCIFLUSH);
-      ret = uart_read(fd2,r_buf,8);
+      ret = uart_read(fd2,r_buf,w_len);
       if(ret == -1)
       {
 	      fprintf(stderr,"uart read failed!\n");
 	      exit(EXIT_FAILURE);
       }
-
-      timer2_ = this->create_wall_timer(
-		      200ms, std::bind(&RadaPublisher::timer_callback2, this));
       publisher2_ = this->create_publisher<ros2_rada_msg::msg::Rada>("rada2", 10);
+      timer2_ = this->create_wall_timer(
+		      100ms, std::bind(&RadaPublisher::timer_callback2, this));
 #endif
     }
 
@@ -86,25 +82,13 @@ class RadaPublisher : public rclcpp::Node
 	    const char *w_buf = (const char *)"11111";
 	    size_t w_len = sizeof(w_buf);
 	    int ret = -1;
-	    char r_buf[11]={0};
-	    //bzero(r_buf,11);
-#if 0
-	    ret = uart_write(fd1,w_buf,w_len);
-	    if(ret == -1)
-	    {
-		    fprintf(stderr,"uart write failed!\n");
-		    exit(EXIT_FAILURE);
-	    }
-#endif
-	    tcflush(fd1,TCIFLUSH);
+	    unsigned char r_buf[10]={0};
 	    ret = uart_read(fd1,r_buf,10);
 	    if(ret == -1)
 	    {
 		    fprintf(stderr,"uart read failed!\n");
 		    exit(EXIT_FAILURE);
 	    }
-	    //tcflush(fd1,TCIOFLUSH);
-
 	    message.r1 = (r_buf[1] << 8) + r_buf[2];
 	    message.r2 = (r_buf[3] << 8) + r_buf[4];
 	    message.r3 = (r_buf[5] << 8) + r_buf[6];
@@ -112,9 +96,9 @@ class RadaPublisher : public rclcpp::Node
 	    message.crc = (r_buf[0] + r_buf[1] + r_buf[2] + r_buf[3] + r_buf[4] + r_buf[5] + r_buf[6] + r_buf[7] + r_buf[8]) & 0x00ff;
 
 	    if (message.crc == r_buf[9]) {
-		    //RCLCPP_INFO(this->get_logger()," Raw1: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x  n: %d ", r_buf[0], r_buf[1], r_buf[2], r_buf[3], r_buf[4], r_buf[5],r_buf[6], r_buf[7], r_buf[8], r_buf[9] ,ret);
-		    //RCLCPP_INFO(this->get_logger(),"CRC1: %04x\n", (r_buf[0] + r_buf[1] + r_buf[2] + r_buf[3] + r_buf[4] + r_buf[5] + r_buf[6] + r_buf[7] + r_buf[8]) & 0x00ff);
-		    //publisher1_->publish(message);
+		    RCLCPP_INFO(this->get_logger()," Raw1: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x  n: %d ", r_buf[0], r_buf[1], r_buf[2], r_buf[3], r_buf[4], r_buf[5],r_buf[6], r_buf[7], r_buf[8], r_buf[9] ,ret);
+		    RCLCPP_INFO(this->get_logger(),"CRC1: %04x\n", (r_buf[0] + r_buf[1] + r_buf[2] + r_buf[3] + r_buf[4] + r_buf[5] + r_buf[6] + r_buf[7] + r_buf[8]) & 0x00ff);
+		    publisher1_->publish(message);
 	    }
 	    else {
 		    RCLCPP_INFO(this->get_logger()," Raw1: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x  n: %d ", r_buf[0], r_buf[1], r_buf[2], r_buf[3], r_buf[4], r_buf[5],r_buf[6], r_buf[7], r_buf[8], r_buf[9] ,ret);
@@ -133,17 +117,7 @@ class RadaPublisher : public rclcpp::Node
 	    const char *w_buf = (const char *)"11111";
 	    size_t w_len = sizeof(w_buf);
 	    int ret = -1;
-	    char r_buf[11] = {0};
-	    //bzero(r_buf,11);
-#if 0
-	    ret = uart_write(fd2,w_buf,w_len);
-	    if(ret == -1)
-	    {
-		    fprintf(stderr,"uart write failed!\n");
-		    exit(EXIT_FAILURE);
-	    }
-#endif
-	    tcflush(fd2,TCIFLUSH);
+	    unsigned char r_buf[10] = {0};
 	    ret = uart_read(fd2,r_buf,10);
 	    if(ret == -1)
 	    {
@@ -159,9 +133,9 @@ class RadaPublisher : public rclcpp::Node
 	    message.crc = (r_buf[0] + r_buf[1] + r_buf[2] + r_buf[3] + r_buf[4] + r_buf[5] + r_buf[6] + r_buf[7] + r_buf[8]) & 0x00ff;
 
 	    if (message.crc == r_buf[9]) {
-		   // RCLCPP_INFO(this->get_logger()," Raw2: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x  n: %d :", r_buf[0], r_buf[1], r_buf[2], r_buf[3], r_buf[4], r_buf[5],r_buf[6], r_buf[7], r_buf[8], r_buf[9] ,ret);
-		   // RCLCPP_INFO(this->get_logger(),"CRC2: %04x\n", (r_buf[0] + r_buf[1] + r_buf[2] + r_buf[3] + r_buf[4] + r_buf[5] + r_buf[6] + r_buf[7] + r_buf[8]) & 0x00ff);
-		   // publisher2_->publish(message);
+		    RCLCPP_INFO(this->get_logger()," Raw2: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x  n: %d :", r_buf[0], r_buf[1], r_buf[2], r_buf[3], r_buf[4], r_buf[5],r_buf[6], r_buf[7], r_buf[8], r_buf[9] ,ret);
+		    RCLCPP_INFO(this->get_logger(),"CRC2: %04x\n", (r_buf[0] + r_buf[1] + r_buf[2] + r_buf[3] + r_buf[4] + r_buf[5] + r_buf[6] + r_buf[7] + r_buf[8]) & 0x00ff);
+		    publisher2_->publish(message);
 	    }
 	    else {
 		    RCLCPP_INFO(this->get_logger(), "2 Rada CRC Wrong!\n");
